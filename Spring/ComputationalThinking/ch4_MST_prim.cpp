b6efdfaffd96a05864eb257a3ad45fee129a229e
isdiscodead <isdiscodead@korea.ac.kr>
2022-05-03 12:08:54 +0900

    MST

--- Spring/ComputationalThinking/ch4_MST_prim.cpp
+++ Spring/ComputationalThinking/ch4_MST_prim.cpp
@@ -27,20 +27,61 @@ void prim( int n, const int W[][SIZE], edge* F ) {
     int* nearest = new int[n+1]; // 0, 1 인덱스는 사용 X ( 0은 무시, 1은 출발지인 v1 )
 
     // vi에서 가장 가까운 노드까지의 비용 
-    int* distance = new int[n];
+    int* distance = new int[n+1];
 
     // nearest, distance 초기화
-    for ( i=2 ; i<n ; i++ ) {
+    for ( i=2 ; i<=n ; i++ ) {
         nearest[i] = 1; // v1로 초기화 
-        distance[i] = W[1][i]; // v1 ~ vi 거리 
+        distance[i] = W[0][i-1]; // v1 ~ vi 거리 
     }
 
-    // 각 정점에서 가까운 거리 찾기 
-    for ( i=2 ; i<=n ; i++ ) { // repeat n-1 times
+    // 각 정점 i에서 가까운 거리 찾기 
+    for ( i=0 ; i<n ; i++ ) { // repeat n-1 times
         min = INF;
 
-        for ( j=1 ; j<n ; j++ ) {
-
+        for ( j=2 ; j<=n ; j++ ) {
+            if ( distance[j] < min && distance[j] >= 0 ) { // 그래프 상에서 짧은 경로 발견 시 
+                min = distance[j];
+                vnear = j;
+            }
         }
+
+        // 추가할 경로 저장 
+        edge new_edge;
+        new_edge.v1 = vnear;
+        new_edge.v2 = nearest[vnear];
+
+        F[f_size] = new_edge;
+        f_size++;
+
+        distance[vnear] = -1; // 이미 방문한 곳은 다시 탐색하지 않도록 ... 
+
+        // 현재 선택한 경유지 사용하여 갈 수 있는 빠른 길이 있다면 update
+        for ( j=2; j<=n ; j++ ) {
+            if ( W[j-1][vnear-1] < distance[j] ) {
+                distance[j] = W[j-1][vnear-1];
+                nearest[j] = vnear;
+            }
+        }
+    }
+}
+
+int main() {
+    int weigth_matrix[SIZE][SIZE] = {
+        { 0, 1, 3, INF, INF },
+        { 1, 0, 3, 6, INF },
+        { 3, 3, 0, 4, 2 },
+        { INF, 6, 4, 0, 5},
+        { INF, INF, 2, 5, 0 }
+    };
+
+    // MST's maximum edge number is n-1
+    edge* F = new edge[SIZE-1];
+
+    prim(SIZE, weigth_matrix, F);
+
+    int i;
+    for ( i=0; i < SIZE-1 ; i++ ) {
+        cout << F[i].v1 << ", " << F[i].v2 << endl;
     }
 }
